5ce4c01671841a8fc415fabfe24068e3917d2092
VIPLOVEBOY420 <157475582+VIPLOVEBOY420@users.noreply.github.com>
2025-03-20 08:02:13 +0530

    Update scrapper.py

--- bot/modules/scrapper.py
+++ bot/modules/scrapper.py
@@ -244,21 +244,21 @@ async def stop_scrapper(_, query: CallbackQuery, obj: ScrapeHelper):
 
 bot.add_handler(MessageHandler(scrapper, filters=command(BotCommands.ScrapperCommand) & CustomFilters.authorized))
 
-@new_task
+#@new_task
 
-async def tamilmv(url):
-    cget = create_scraper().request
-    resp = cget("GET", url)
-    soup = BeautifulSoup(resp.text, "html.parser")
-    mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
-    tor = soup.select('a[data-fileext="torrent"]')
-    parse_data = f"<b><u>{soup.title.string}</u></b>"
+#async def tamilmv(url):
+   # cget = create_scraper().request
+   # resp = cget("GET", url)
+   # soup = BeautifulSoup(resp.text, "html.parser")
+    #mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
+   # tor = soup.select('a[data-fileext="torrent"]')
+   # parse_data = f"<b><u>{soup.title.string}</u></b>"
     
-    for no, (t, m) in enumerate(zip(tor, mag), start=1):
-        filename = sub(r"www\S+|\- |\.torrent", "", t.string)
-        parse_data += f"""
+  #  for no, (t, m) in enumerate(zip(tor, mag), start=1):
+     #   filename = sub(r"www\S+|\- |\.torrent", "", t.string)
+    #    parse_data += f"""
         
-{no}. <code>{filename}</code>
-┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>"""
+#{no}. <code>{filename}</code>
+#┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>"""
     
-    return parse_data 
+#    return parse_data 
