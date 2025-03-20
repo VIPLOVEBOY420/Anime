c6f2cd392249b5a8d689a45e77670ce609ee8012
VIPLOVEBOY420 <157475582+VIPLOVEBOY420@users.noreply.github.com>
2025-03-20 07:27:17 +0530

    Update scrapper.py

--- bot/modules/scrapper.py
+++ bot/modules/scrapper.py
@@ -178,6 +178,21 @@ class ScrapeHelper():
                 await self._onScrapSuccess(len(links), mode)
             else:
                 await self._onScrapError('ERROR: Can\'t find any link!')
+                
+async def tamilmv(url):
+    cget = create_scraper().request
+    resp = cget("GET", url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
+    tor = soup.select('a[data-fileext="torrent"]')
+    parse_data = f"<b><u>{soup.title.string}</u></b>"
+    for no, (t, m) in enumerate(zip(tor, mag), start=1):
+        filename = sub(r"www\S+|\- |\.torrent", "", t.string)
+        parse_data += f"""
+        
+{no}. <code>{filename}</code>
+┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>""
+    return parse_data
 
     async def txt_file(self):
         self._event_handler()
